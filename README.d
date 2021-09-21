bd3ab13a27c3a631b9cb42903a0b71187b8a2eac
Anran31 <55323042+Anran31@users.noreply.github.com>
2021-09-21 20:35:40 +0700

    Create README.md

---
+++ README.d
@@ -0,0 +1,16 @@
+# Tugas 1 - Grafika Komputer B
+
+
+NRP              | Nama
+-----------------|-----------
+05111940000031   | Andika Nugrahanto 
+
+Benda: 
+
+### Foto Benda Depan Atas
+
+### Foto Benda Kanan Atas
+
+### Foto Benda Belakang Atas
+
+### Foto Benda Kiri Atas
